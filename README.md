e5e0d5aa5f076ebefdadf0893f0c9c530570ad7d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-12-22 16:04:25 +0100

    Started work on a generic implementation but left it....got other things to do. Look at status on previous commits for a working version.

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ So, this is a simple rust program that finds them and prints them out.
 
 ## To run
 Clone or download the repo, `cd` into the folder.
- 
+
+To use `#![feature(step_trait)]` you need nightly, so set anb override using `rustup override set nightly`
+
 Build and run using `cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-fn main() {
-    let mut float32: f32;
-    let mut rt_int32: u32;
+#![feature(step_trait)]
+
+use std::iter::Step;
+use std::fmt::Display;
+use std::convert::From;
+
+// As not all the possible type conversions from integers to floats are implemented by the primitive types
+// we would need to implement the conversions here using traits - but it falls foul of Orphan rules for Trait implementations
+// https://doc.rust-lang.org/error-index.html#E0117
+// So this needs fixing
+impl From<f32> for u16 {
+    fn from(float: f32) -> Self {
+        float as u16
+    }
+}
+
+fn check_conversion<Original: Step + Display + From<To>, To: Display + From<Original>>(min: Original, max: Original, _fake: To, from_type: &str, to_type: &str) {
+    let mut converted: To;
+    let mut converted_back: Original;
     let mut count: usize = 0;
 
-    // Do for u32 first
-    println!("Checking u32 from {} to {}", 0, std::u32::MAX);
-    for int32 in 0_u32..std::u32::MAX {
-        float32 = int32 as f32;
-        rt_int32 = float32 as u32;
-        if int32 != rt_int32 {
-            println!("{} in u32 is {} in f32", int32, float32);
+    println!("Checking from {} to {}", from_type, to_type);
+    for original in min..max {
+        converted = To::from(original);
+        converted_back = Original::from(converted);
+        if original != converted_back {
+            println!("{} in {} is {} in {}", original, from_type, converted, to_type);
             count += 1;
         }
     }
     println!("total count that do not match between u32 and f32 is {}", count);
+}
+
+fn main() {
+    // Do for u16 to f32 first
+    let min:u16 = std::u16::MIN;
+    let max: u16 = std::u16::MAX;
+    let fake: f32 = 0.0;
+    check_conversion(min, max, fake, "u32", "f32" )
+
 }
